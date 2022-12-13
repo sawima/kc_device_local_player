0be6174644618e9e0844e388f2eca4fabd5be64b
James <james@kimatech.com>
2022-12-13 15:28:28 +0800

    prod_init_version

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,12 @@
-export const config = {
-    // localDefaultAssetImage:"assets/kimacloud1920x1080.jpg",
-    remoteDefaultAssetImage:"https://kcsignage-dev.1m2.fun/publicAssets/placeholder/kimacloud1920x1080.jpg",
+// export const config = {
+//     remoteDefaultAssetImage:"https://kcsignage-dev.1m2.fun/publicAssets/placeholder/kimacloud1920x1080.jpg",
 
+//     localAssetSite:"http://localhost:8081/assets/",
+//     remoteAssetSite:"https://kcsignage-dev.1m2.fun/"
+// }
+
+export const config = {
+    remoteDefaultAssetImage:"https://kcsignage-prod.kimacloud.online/publicAssets/placeholder/kimacloud1920x1080.jpg",
     localAssetSite:"http://localhost:8081/assets/",
-    remoteAssetSite:"https://kcsignage-dev.1m2.fun/"
+    remoteAssetSite:"https://kcsignage-prod.kimacloud.online/"
 }
